5ad5d7bcc2dd0d44352df4ae0208c1ce1bd4a789
TheMiCraft <TheMiCraft@users.noreply.github.com>
2022-07-25 20:26:25 +0200

    init

---
+++ main.py
@@ -0,0 +1,42 @@
+import discord
+
+intents = discord.Intents.default()
+# This sets the intents to the default intents of discord.
+intents.message_content = True
+# This allowes the bot to view the content of messages
+
+client = discord.Bot(intents=intents)
+# Creates the bot with the intents
+
+TOKEN = 'OTk4ODk2OTk3MjkxNTMyMzE4.GnLQQ2.a7eYid4IQeARoQVztATDg8uWf1-h2sxJ88wu3w'
+# This sets the variable TOKEN with your token
+
+
+@client.event
+# This calls the event listener of py-cord to listen to the on_ready event and when its executed to run the code
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    # This will be printed when the Bot has successfully connected to Discord
+
+
+@client.event
+# This calls the event listener of py-cord to listen to the on_message event and when its executed to run the code
+async def on_message(message):
+    if message.content == 'ping':
+        # This is checking if the message equals  "ping"
+        channel = message.channel
+        # This gets the channel from discord and puts it into a variable
+        await channel.send('pong!')
+        # This is responding "pong" to the message
+
+
+@client.slash_command(name='ping', description='Ping!')
+# This calls the slash command manager of py-cord to create a new command with the name ping and description "Ping!"
+# and when the command is executed to run the code
+async def ping(ctx):
+    await ctx.respond(f"Pong!")
+    # This is responding "Pong!" to the command
+
+
+client.run(TOKEN)
+# This will start the Bot
